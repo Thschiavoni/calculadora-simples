0fa6e19497c1275d8a7c90a54b7278eb6303f1b5
Thschiavoni <trevas16122003@gmail.com>
2021-09-16 22:37:50 -0300

    adding simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -3,4 +3,4 @@ import a as a
 n1 = int(input('escolha um numero: '))
 n2 = int(input('escolha outro numero: '))
 s = n1 + n2
-print('a soma de {} mais {} e igual a {}'.format(n1, n2, s))
+print(f'a soma de {n1} mais {n2} e igual a {s}')
